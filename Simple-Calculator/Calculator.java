04f5b43a0e3e73e70485ec6a370d8b390ca2432f
utkarsh <123744256+um26@users.noreply.github.com>
2025-08-12 23:54:56 +0530

    Update Calculator.java

--- Simple-Calculator/Calculator.java
+++ Simple-Calculator/Calculator.java
@@ -9,6 +9,7 @@ public class Calculator extends JFrame implements ActionListener{
   private JButton addButton, subButton, mulButton, divButton, equalsButton, clearButton;
 
   
+
   private double num1, num2, result;
   private char operator;
 
